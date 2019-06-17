cec050207338408977553bbe7ec82b195786a1ff
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-06-17 14:50:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("590156024808210469")
 setInterval(function() {
-channel.send(`spaaaaaaaaaaaaaaaaaaaaaaaaam spam سبااااااااااااااااااااااااااااااااااااااااام سبام`);
+channel.send(`Immmmmmmmmmm SsaaaaaaaaaaaaaaadD`);
 }, 30)
 })
 
